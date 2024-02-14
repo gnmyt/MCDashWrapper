d790348347c186e5dd165fb23487ad243520e6ba
Mathias Wagner <germannewsmaker@gmail.com>
2024-02-15 00:54:59 +0100

    Updated the App.jsx

--- webui/src/App.jsx
+++ webui/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import i18n from "./i18n.js";
+import {useState} from "react";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Login} from "@mui/icons-material";
+import {routes} from "@/common/routes/server.jsx";
+import Root from "@/states/Root/index.js";
+import {TokenProvider} from "@/common/contexts/Token/index.js";
+import {ServerProvider} from "@/common/contexts/Server/index.js";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default () => {
+    const [translationsLoaded, setTranslationsLoaded] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const router = createBrowserRouter([
+        {path: "/login", element: <Login />},
+        {path: "/", element: <ServerProvider><Root /></ServerProvider>, children: routes}
+    ]);
+
+    i18n.on("initialized", () => setTranslationsLoaded(true));
+
+    if (!translationsLoaded) return <></>;
+
+    return (
+        <>
+            <TokenProvider>
+                <RouterProvider router={router} />
+            </TokenProvider>
+        </>
+    )
 }
-
-export default App
