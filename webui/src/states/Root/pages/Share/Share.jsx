cd06c21c941bda71f8d7150e675c5eca268609e4
Mathias Wagner <germannewsmaker@gmail.com>
2024-02-15 00:55:23 +0100

    Created the Share.jsx page

---
+++ webui/src/states/Root/pages/Share/Share.jsx
@@ -0,0 +1,10 @@
+import {Typography} from "@mui/material";
+import {t} from "i18next";
+
+export const Share = () => {
+    return (
+        <div>
+            <Typography>{t("coming_soon")}</Typography>
+        </div>
+    )
+}
