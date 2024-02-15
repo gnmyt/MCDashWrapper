33b9480294956e04cbfc5ad18dbc728a7ad86b97
Mathias Wagner <germannewsmaker@gmail.com>
2024-02-15 16:40:50 +0100

    Integrated the Login state into the App.jsx

--- webui/src/App.jsx
+++ webui/src/App.jsx
@@ -1,11 +1,11 @@
 import i18n from "./i18n.js";
 import {useState} from "react";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {Login} from "@mui/icons-material";
 import {routes} from "@/common/routes/server.jsx";
 import Root from "@/states/Root/index.js";
-import {TokenProvider} from "@/common/contexts/Token/index.js";
-import {ServerProvider} from "@/common/contexts/Server/index.js";
+import {TokenProvider} from "@/common/contexts/Token";
+import {ServerProvider} from "@/common/contexts/Server";
+import Login from "@/states/Login";
 
 export default () => {
     const [translationsLoaded, setTranslationsLoaded] = useState(false);
