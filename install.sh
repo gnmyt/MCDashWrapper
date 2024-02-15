63081f8919f05b364f405b89392d337af336ae8c
Mathias Wagner <germannewsmaker@gmail.com>
2024-02-15 21:02:54 +0100

    Updated the install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ function error() {
 
 function download() {
     if wget -S --spider "$1" 2>&1 | grep -q 'HTTP/1.1 200 OK'; then
-        wget -q --show-progress "$2" "$1"
+        wget -q --show-progress -O "$2" "$1"
     fi
 }
 
