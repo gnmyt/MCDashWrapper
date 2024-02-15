14c662a646f92ec8bbdc6844c0acd8dade404292
Mathias Wagner <germannewsmaker@gmail.com>
2024-02-15 18:27:45 +0100

    Updated the main.jsx

--- webui/src/main.jsx
+++ webui/src/main.jsx
@@ -6,15 +6,13 @@ import '@fontsource/inter/300.css';
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
 import '@fontsource/inter/700.css';
-import {CssBaseline, ThemeProvider} from "@mui/material";
 
-import theme from "@/common/themes/default.js";
+import {SettingsProvider} from "@/common/contexts/Settings";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <App/>
-        </ThemeProvider>
+        <SettingsProvider>
+            <App />
+        </SettingsProvider>
     </React.StrictMode>
 );
